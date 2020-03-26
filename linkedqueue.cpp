fc23790bbad8ee459941a672ec60246eb8deb53b
Sarthak1306 <sarthak13062001@gmail.com>
2020-03-26 11:56:37 +0530

    Linked Queue - Insert, Delete Fuction Added

---
+++ linkedqueue.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+struct node {
+   int data;
+   struct node *next;
+};
+struct node* front = NULL;
+struct node* rear = NULL;
+struct node* temp;
+void Insert() {
+   int val;
+   cout<<"Insert the element in queue : "<<endl;
+   cin>>val;
+   if (rear == NULL) {
+      rear = (struct node *)malloc(sizeof(struct node));
+      rear->next = NULL;
+      rear->data = val;
+      front = rear;
+   } else {
+      temp=(struct node *)malloc(sizeof(struct node));
+      rear->next = temp;
+      temp->data = val;
+      temp->next = NULL;
+      rear = temp;
+   }
+}
+void Delete() {
+   temp = front;
+   if (front == NULL) {
+      cout<<"Underflow"<<endl;
+      return;
+   }
+   else
+   if (temp->next != NULL) {
+      temp = temp->next;
+      cout<<"Element deleted from queue is : "<<front->data<<endl;
+      free(front);
+      front = temp;
+   } else {
+      cout<<"Element deleted from queue is : "<<front->data<<endl;
+      free(front);
+      front = NULL;
+      rear = NULL;
+   }
+}
