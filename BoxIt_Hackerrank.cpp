1b218c3ce46eac21fd79ffc5d99b7c218d96d0bb
Sarthak1306 <sarthak13062001@gmail.com>
2020-03-31 12:57:30 +0530

    Reverted Shivansh Commit to Master Branch

--- BoxIt_Hackerrank.cpp
+++ BoxIt_Hackerrank.cpp
@@ -34,16 +34,4 @@ class Box{
   // Yet to add Bool Functions for friend and osteam operator
 
   // Shivansh add bool friend function and bool ostream operator function here
-  friend bool operator < ( Box&A,Box& B){
-        if( (A.l < B.l)  ((A.b < B.b) && (A.l == B.l))  ((A.h < B.h) && (A.l == B.l) && (A.b == B.b)) ){
-            return true;
-        }else{
-            return false;
-        }
-    };
-
-    friend ostream& operator<< (ostream& output, const Box& B){
-        output << B.l << " " << B.b << " " << B.h;
-        return output;
-    }
-};
+  
